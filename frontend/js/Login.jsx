b5fe25d071dfb41d756ada1c78dfe2060bae823f
kytran <kytranl0@yahoo.com>
2018-07-18 16:37:47 -0400

    login page is prettier

--- frontend/js/Login.jsx
+++ frontend/js/Login.jsx
@@ -14,12 +14,27 @@ export default class Login extends React.Component{
 
         })
     }
-
+// <div>
+// <button type="button" className="btn btn-primary btn-lg btn-block">
+// Google Calendar
+// </button>
+// <button type="button" className="btn btn-outline-secondary btn-lg btn-block" onClick={this.login}>
+// Google Login
+// </button>
+// </div>
     render() {
         return (
-            <div>
-                <button onClick={this.login}>Google Login</button>
+
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="font-weight-bold text-primary display-1">Google Calendar</h1>
+                <hr class="my-4"></hr>
+                <p className="lead">Welcome to my web-application! Please click the button below me to sign in :D</p>
+                <button type="button" className="btn btn-outline-primary btn-lg btn-block" onClick={this.login}>
+                Google Login
+                </button>
             </div>
+        </div>
         )
     }
 }
